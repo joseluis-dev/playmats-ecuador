96b7a21568ad8f81670d8af346e3b578cb518332
Jose Yepez <jl.yg91@gmail.com>
2025-04-22 19:42:45 -0500

    feat: implement header, cart, and menu components with theme toggle functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  
   integrations: [react()],
 
   vite: {
---
+++ src/components/Cart.tsx
@@ -0,0 +1,10 @@
+import { CartIcon } from "./icons/CartIcon"
+
+export const Cart = () => {
+  return (
+    <button className="relative hover:bg-[var(--color-surface)] transition-all duration-300 ease-in-out rounded-md p-2">
+      <span className="absolute flex justify-center items-center -top-1 -right-1 text-xs rounded-full bg-[var(--color-primary)] text-[var(--btn-primary-text)] w-5 h-5">1</span>
+      <CartIcon />
+    </button>
+  )
+}
