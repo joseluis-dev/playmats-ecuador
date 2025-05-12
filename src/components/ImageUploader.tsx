26cacb41dd98187e0e06b24dd8529822dbe3fed6
Jose Yepez <jl.yg91@gmail.com>
2025-05-12 00:04:48 -0500

    feat: Enhance ImageUploader component with drag-and-drop functionality; add event handlers for file drop and drag over

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -49,6 +49,31 @@ export const ImageUploader = () => {
     }
   }
 
+  const handleDrop = (e: React.DragEvent<HTMLLabelElement>, field: ControllerRenderProps<{ comprobante: File }, "comprobante">) => {
+    e.preventDefault()
+    e.stopPropagation()
+    
+    const file = e.dataTransfer.files[0]
+    field.onChange(file)
+    if (file) {
+      // Validar con Zod antes de crear la URL
+      const result = formSchema.shape.comprobante.safeParse(file)
+      if (result.success) {
+        const objectUrl = URL.createObjectURL(file)
+        setUrl(objectUrl)
+      } else {
+        setUrl(null)
+      }
+    } else {
+      setUrl(null)
+    }
+  }
+
+  const handleDragOver = (e: React.DragEvent<HTMLLabelElement>) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
   }
@@ -64,6 +89,8 @@ export const ImageUploader = () => {
               <FormControl>
                 <label
                   className="border-1 border-dashed border-[var(--color-text)] rounded-md p-4 flex flex-col items-center justify-center cursor-pointer hover:border-blue-500/80 dark:hover:border-blue-400 transition-all duration-200 ease-in-out h-52"
+                  onDrop={(e) => handleDrop(e, field)}
+                  onDragOver={handleDragOver}
                 >
                   {url ? (
                     <img
