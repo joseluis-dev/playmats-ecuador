25b9079d9d5712dac128daf554c0327efe98fdc0
joseluis-dev <jl.yg91@gmail.com>
2025-07-23 15:34:58 -0500

    feat: implement PaymentForm component for file upload and validation; update import path in payment page

--- src/components/AccordionCustom/AccordionDynamic.tsx
+++ src/components/AccordionCustom/AccordionDynamic.tsx
@@ -4,8 +4,6 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
-import type React from "react"
-import { useState } from "react"
 
 interface AccordionDynamicProps {
   items: [
@@ -21,7 +19,7 @@ export const AccordionDynamic = ({ items = [] }: AccordionDynamicProps) => {
     <Accordion type="multiple" className="w-full flex flex-col gap-2">
       {items.map((item, index) => (
         <AccordionItem key={index} value={`item-${index}`}>
-          <AccordionTrigger className="flex items-center hover:no-underline hover:bg-[var(--color-surface)] px-4 border border-[var(--color-text)]">{typeof item.trigger === "function" ? item.trigger(item) : item.trigger}</AccordionTrigger>
+          <AccordionTrigger className="flex items-center hover:no-underline hover:bg-[var(--color-surface)] px-4 ">{typeof item.trigger === "function" ? item.trigger(item) : item.trigger}</AccordionTrigger>
           <AccordionContent
             className="px-4 py-2"
           >{typeof item.children === 'function' ? item.children(item) : item.children}</AccordionContent>
