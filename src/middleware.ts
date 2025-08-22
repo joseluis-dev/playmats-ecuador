39c370ab444f4be6e11a6812e397a1d231d40e01
joseluis-dev <jl.yg91@gmail.com>
2025-08-22 17:01:02 -0500

    feat: Integrate Clerk authentication components in Header; add middleware for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/admin(/.*)?",
+  "/profile"
+])
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  const { userId, redirectToSignIn } = auth()
+  
+  if (isProtectedRoute(context.request) && !userId) {
+    return redirectToSignIn();
+  }
+});
