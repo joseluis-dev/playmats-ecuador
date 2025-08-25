2103e2e900cff83a43ac88dd8898ed7fd78b4fa6
joseluis-dev <jl.yg91@gmail.com>
2025-08-25 17:00:42 -0500

    feat: Update Cart and Header components for improved layout; adjust Menu button styling and enhance middleware for route protection

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,7 +5,7 @@ export const Cart = () => {
   const { totalItems } = useCart()
   return (
     <a href="/cart" className="relative hover:bg-[var(--color-surface)] transition-all duration-300 ease-in-out rounded-md p-2 z-100">
-      <span className="absolute flex justify-center items-center -top-1 -right-1 text-xs rounded-full bg-[var(--color-primary)] w-5 h-5 text-[var(--color-text)]">{totalItems}</span>
+      <span className="absolute flex justify-center items-center top-0 right-0 text-xs rounded-full bg-[var(--color-primary)] w-5 h-5 text-[var(--color-text)]">{totalItems}</span>
       <CartIcon className="text-[var(--color-text)]"/>
     </a>
   )
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/admin(/.*)?",
-  "/profile"
+  "/admin(/.*)?"
 ])
 
 export const onRequest = clerkMiddleware((auth, context) => {
   const { userId, redirectToSignIn } = auth()
-  
+
   if (isProtectedRoute(context.request) && !userId) {
     return redirectToSignIn();
   }
