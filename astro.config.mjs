9a060e64e0887910e5e6f1a9627f480544719d52
joseluis-dev <jl.yg91@gmail.com>
2025-08-27 09:30:42 -0500

    feat: Integrate @clerk/themes for improved UI theming; update Header and Video components for better accessibility and navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,17 @@ import clerk from "@clerk/astro";
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
+import { shadcn, dark } from '@clerk/themes'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   
-  integrations: [react(), clerk()],
+  integrations: [react(), clerk({
+    appearance: {
+      theme: shadcn,
+    }
+  })],
 
   vite: {
     plugins: [tailwindcss()]
